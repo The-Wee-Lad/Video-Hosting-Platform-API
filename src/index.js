d8e0ef4322182db1294c515e30b7e867b32d14b4
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-01-08 05:47:53 +0530

    added Utilities

--- src/index.js
+++ src/index.js
@@ -1,49 +1,18 @@
-// require('dotenv').config({path:'./.env'});
-
 import connectDB from "../db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./.env'
 })
-connectDB();
 
 
-
-
-/*
-import mongoose, { connect } from "mongoose";
-import { DB_NAME } from "./constants";
-// async function connectDB(){}
-
-// connectDB();
-import express from "express";
-const app = express();
-
-
-
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("Error",(error)=>{
-            console.log("Err: ",error);
-            throw error;
-        });
-
-        app.listen(process.env.PORT, ()=> {
-            console.log(`Listening at : ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.err("ERROR : ", error);
-        throw error;
-    }
-})()
-
-new Promise((resolveOuter) => {
-    resolveOuter(
-        new Promise((resolveInner) => {
-            setTimeout(resolveInner, 1000);
-        }),
-    );
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT||3000, ()=> {
+        console.log(`Listening at : ${process.env.PORT||3000}`);
+    })
+})
+.catch((err) => {
+    console.log("DB CONNECTION FAIL : ",err);
 });
-*/
